b8fee92f1a800743eec835a1b03ec81f3dbb7643
Sangeeth Nandakumar <sangeethnandakumarwork@gmail.com>
2020-09-10 01:41:49 +0530

    Minor Optimisations

--- Demo/ExpressGlobalExceptionHandler/ExceptionExtensions.cs
+++ Demo/ExpressGlobalExceptionHandler/ExceptionExtensions.cs
@@ -7,17 +7,6 @@ namespace ExpressGlobalExceptionHandler
 {
     public static class ExceptionExtensions
     {
-        //public static void ConfigureGlobalExceptionHandler(this IApplicationBuilder app)
-        //{
-        //    app.UseExceptionHandler(builder =>
-        //    {
-        //        builder.Run(async context =>
-        //        {
-        //            context.Response.Redirect("/Home/Error");
-        //        });
-        //    });
-        //}
-
         public static void UseGlobalExceptionHandler(this IApplicationBuilder app)
         {
             app.UseMiddleware<ExceptionMiddleware>();
